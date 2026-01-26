88a829a0fe772a49e9a041f84aca960ed5c4bba7
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-01-26 16:26:41 +0530

    Time: 0 ms (100%), Space: 16.6 MB (37.79%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,50 +10,26 @@
  */
 class Solution {
 public:
-    ListNode* reverseLL(ListNode* head){
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-        while(temp != NULL){
-            ListNode* front = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = front;
-        }
-        return prev;
-    }
-    ListNode* findkthnode(ListNode* temp,int k){
-        k -= 1;
-        while(temp != NULL && k>0){
-            k--;
-            temp = temp->next;
-        }
-        return temp;
-        
-    }
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* temp = head;
-        ListNode* prevlast = NULL;
-        while(temp != NULL ){
-            ListNode* kthnode = findkthnode(temp,k);
-            if(kthnode == NULL){
-                if(prevlast){
-                    prevlast->next = temp;
-                }
-                break;
+        int cnt = 0;
+        while(cnt < k){
+            if(temp == NULL){
+                return head;
             }
-            ListNode* nextnode = kthnode->next;
-            kthnode->next = NULL;
-            reverseLL(temp);
-            if(temp == head){
-                head = kthnode;
-            }
-            else{
-                prevlast->next = kthnode;
-            }
-            prevlast = temp;
-            temp = nextnode;
-         }
-         return head;
-        
+            cnt++;
+            temp=temp->next;
+        }
+        ListNode* prev = reverseKGroup(temp,k);
+        temp = head;
+        cnt = 0;
+        while(cnt < k){
+            ListNode* nextNode = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = nextNode;
+            cnt++;
+        }
+        return prev;
     }
 };
