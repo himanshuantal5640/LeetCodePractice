3cdfc3eb96b594792ac6039fdf2fd0e53c90f4f0
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-01-29 22:17:35 +0530

    Time: 0 ms (100%), Space: 10.4 MB (61.56%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        int n = tickets.size();
+        if(tickets[k] == 1){
+            return k+1;
+        }
+        while(tickets[k] > 0){
+            for(int i=0;i<n;i++){
+                if(tickets[i] != 0){
+                    tickets[i]--;
+                    time++;
+                }
+                if(tickets[k] == 0){
+                    return time;
+                }
+            }
+        }
+        return time;
+    }
+};
