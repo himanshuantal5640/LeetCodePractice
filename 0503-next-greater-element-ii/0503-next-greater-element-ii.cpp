536882cdc6c309a08224eebf1fd915005f8691f4
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-01-29 23:42:32 +0530

    Time: 0 ms (100%), Space: 28.5 MB (58.93%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,10 +5,10 @@ public:
         int n = nums.size();
         vector<int> ans(n,-1);
         for(int i = 2*n-1;i>=0;i--){
-            while(!s.empty() && nums[s.top()] <= nums[i%n]){
+            while(!s.empty() && nums[s.top()]<= nums[i%n]){
                 s.pop();
             }
-            ans[i%n] = !s.empty() ? nums[s.top()] : -1;
+            ans[i%n] = !s.empty()? nums[s.top()]:-1;
             s.push(i%n);
         }
         return ans;
