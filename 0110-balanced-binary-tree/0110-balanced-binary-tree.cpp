f7218856ce55dcbe19fa0d7b4c7c56cedadedd06
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-02-08 08:13:05 +0530

    Time: 0 ms (100%), Space: 22.8 MB (97.22%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -16,18 +16,17 @@ public:
             return 0;
         }
         int lh = height(root->left);
-        if(lh == -1 ){
+        if(lh == -1){
             return -1;
         }
         int rh = height(root->right);
         if(rh == -1){
             return -1;
         }
-        if(abs(rh - lh) > 1){
+        if(abs(lh - rh) > 1){
             return -1;
         }
         return max(lh,rh) + 1;
-
     }
     bool isBalanced(TreeNode* root) {
         return height(root) != -1;
