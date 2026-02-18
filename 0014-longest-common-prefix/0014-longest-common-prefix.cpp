6658ccb6de58402b01d8c5f41528e6bd431c4da7
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-02-18 19:10:11 +0530

    Time: 0 ms (100%), Space: 12.1 MB (29.46%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,10 +4,10 @@ public:
         if(strs.size() == 0){
             return "";
         }
+        string ans = "";
         sort(strs.begin(),strs.end());
         string f = strs[0];
-        string l = strs[strs.size() - 1];
-        string ans = "";
+        string l = strs[strs.size()-1];
         int n = min(f.size(),l.size());
         for(int i=0;i<n;i++){
             if(f[i] != l[i]){
