2de8b33c37d241144ee619f8fa8d56eeb7147671
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-01 16:26:38 +0530

    Time: 7 ms (93.41%), Space: 34.7 MB (83.83%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> dist(n,vector<int>(m,-1));
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 0){
+                    q.push({i,j});
+                    dist[i][j] = 0;
+                }
+            }
+        }
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr >= 0 && nr < n && nc >=0 && nc < m && dist[nr][nc] == -1){
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.push({nr,nc});
+                }
+            }
+        }
+        return dist;
+    }
+};
