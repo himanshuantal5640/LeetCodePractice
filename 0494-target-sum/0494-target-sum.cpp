4dfe9afe24f6aa07d4c71f88f4951274155fe7a7
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-03 00:04:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum = 0;
-        for(int num : nums) totalSum += num;
-
-        if(abs(target) > totalSum) return 0;
-        if((totalSum + target) % 2 != 0) return 0;
-
-        int subsetSum = (totalSum + target) / 2;
-
-        vector<int> dp(subsetSum + 1, 0);
-        dp[0] = 1;  // One way to make sum 0
-
-        for(int num : nums) {
-            for(int j = subsetSum; j >= num; j--) {
-                dp[j] += dp[j - num];
+        int sum = 0;
+        for(int n:nums){
+            sum += n;
+        }
+        if(abs(target) > sum){
+            return 0;
+        }
+        if((target + sum)%2 != 0){
+            return 0;
+        }
+        int sub = (target+sum)/2;
+        vector<int> dp(sub + 1,0);
+        dp[0] = 1;
+        for(int n:nums){
+            for(int j=sub;j>=n;j--){
+                dp[j] += dp[j-n];
             }
         }
-
-        return dp[subsetSum];
+        return dp[sub];
     }
 };
