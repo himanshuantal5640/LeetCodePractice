8fe0414f113f9758230464f1ebae450b11935a96
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-03 19:04:04 +0530

    Time: 0 ms (100%), Space: 13.3 MB (54.48%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -7,14 +7,14 @@ public:
         if(dp[idx] != -1){
             return dp[idx];
         }
-        //1 day pass
+        // 1 day pass
         int op1 = costs[0] + solve(idx+1,days,costs,dp);
-        //7 days pass;
+        // for 7 days pass
         int i;
-        for(i = idx;i<days.size() && days[i] < days[idx]+7;i++);
+        for(i = idx;i<days.size() && days[i] < days[idx] + 7;i++);
         int op2 = costs[1] + solve(i,days,costs,dp);
-        // 30 days pass
-        for(i = idx;i<days.size() && days[i] < days[idx]+30;i++);
+        //30 days pass
+        for(i = idx;i<days.size() && days[i] < days[idx] + 30;i++);
         int op3 = costs[2] + solve(i,days,costs,dp);
         dp[idx] = min(op1,min(op2,op3));
         return dp[idx];
