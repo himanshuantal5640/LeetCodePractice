8a3b31ec0f6b72cab0cd07ade108b92d626fa353
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-03 11:47:37 +0530

    Time: 7 ms (68.51%), Space: 15.5 MB (48.64%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
+    int helper(int i,int tar,vector<vector<int>> &dp,vector<int> &nums){
+        if(i >= nums.size()){
+            return tar == 0 ? 1 : 0;
+        }
+        if(dp[i][tar] != -1){
+            return dp[i][tar];
+        }
+        int notTake = helper(i+1,tar,dp,nums);
+        int take = 0;
+        if(tar >= nums[i]){
+            take = helper(i+1,tar-nums[i],dp,nums);
+        }
+        return dp[i][tar] = take + notTake;
+
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
         int sum = 0;
         for(int n:nums){
             sum += n;
         }
-        if(abs(target)>sum){
+        if(abs(target) > sum ){
             return 0;
         }
-        if((target+sum)%2 != 0){
+        if((target + sum) %2 != 0){
             return 0;
         }
-        int s = (target+sum)/2;
-        vector<int> dp(s+1,0);
-        dp[0] = 1;
-        for(int n:nums){
-            for(int j=s;j>=n;j--){
-                dp[j] += dp[j-n];
-            }
-        }
-        return dp[s];
+        int newTar = (target + sum)/2;
+        vector<vector<int>> dp(n,vector<int>(newTar+1,-1));
+        return helper(0,newTar,dp,nums);
     }
 };
