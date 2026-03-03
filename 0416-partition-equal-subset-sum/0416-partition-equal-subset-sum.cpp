6ee86cda9261ca8858c449e674883287b6a10eb4
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-03 11:32:29 +0530

    Time: 175 ms (20.37%), Space: 110.6 MB (25.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
+    bool helper(int i,int tar,vector<vector<int>> &dp,vector<int> &nums){
+        if(i >= nums.size()){
+            return false;
+        }
+        if(tar == 0){
+            return true;
+        }
+        if(dp[i][tar] != -1){
+            return dp[i][tar];
+        }
+        bool notTake = helper(i+1,tar,dp,nums);
+        bool take = false;
+        if(tar >= nums[i]){
+            take = helper(i+1,tar - nums[i],dp,nums);
+        }
+        dp[i][tar] = take || notTake;
+        return dp[i][tar];
+
+    }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(int n:nums){
+        int n = nums.size();
+        for(int n : nums){
             sum += n;
-        }   
+        }
         if(sum % 2 != 0){
             return false;
         }
-        int t = sum/2;
-        vector<bool> dp(t+1,false);
-        dp[0] = true;
-        for(int n:nums){
-            for(int j=t;j>=n;j--){
-                dp[j] = dp[j] || dp[j-n];
-            }
-        }
-        return dp[t];
+        int tar = sum/2;
+        vector<vector<int>> dp(n,vector<int>(tar+1,-1));
+        return helper(0,tar,dp,nums);
     }
 };
