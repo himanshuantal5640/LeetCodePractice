d6a2d50e4ac4537b4477e59c8c799ce033e14aec
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-06 14:47:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     bool solve(string &s, string &p, int i, int j, vector<vector<int>> &dp){
-        
-        // base case
-        if(i < 0 && j < 0) return true;
-
-        if(i >= 0 && j < 0) return false;
-
+        if(i < 0 && j < 0){
+            return true;
+        }
+        if(i >= 0 && j < 0){
+            return false;
+        }
         if(i < 0 && j >= 0){
-            for(int k = 0; k <= j; k++){
-                if(p[k] != '*') return false;
+            for(int k = 0;k<= j;k++){
+                if(p[k] != '*'){
+                    return false;
+                }
             }
             return true;
         }
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        // match
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        //match
         if(s[i] == p[j] || p[j] == '?'){
             return dp[i][j] = solve(s,p,i-1,j-1,dp);
         }
-
-        // star case
         else if(p[j] == '*'){
-            return dp[i][j] = solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
+            return dp[i][j] = ( solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp) ); 
         }
-
-        // not match
         else{
             return dp[i][j] = false;
         }
     }
-
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size(), vector<int>(p.size(), -1));
-        return solve(s,p,s.size()-1,p.size()-1,dp);
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1)); 
+        return solve(s,p,n-1,m-1,dp);  
     }
 };
