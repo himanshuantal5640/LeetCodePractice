0234ab08c436da3cdb90dc51aaabe56f3ae21d91
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-18 18:34:24 +0530

    Time: 0 ms (100%), Space: 13.9 MB (98.54%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void getAllCombinations(int idx,vector<int> &arr,int tar,vector<int> &combine,vector<vector<int>> &ans){
+        if(tar == 0){
+            ans.push_back(combine);
+            return;
+        }
+        for(int i=idx;i<arr.size();i++){
+            if(i > idx && arr[i] == arr[i-1]){
+                continue;
+            }
+            if(arr[i] > tar){
+                break;
+            }
+            combine.push_back(arr[i]);
+            getAllCombinations(i+1,arr,tar-arr[i],combine,ans);
+            combine.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> combine;
+        getAllCombinations(0,candidates,target,combine,ans);
+        return ans;
+    }
+};
