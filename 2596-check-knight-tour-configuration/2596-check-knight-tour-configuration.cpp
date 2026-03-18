fb6eaa48f21bffab4385ed9dd8f44f788b5ee8e0
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-18 20:00:25 +0530

    Time: 3 ms (9.86%), Space: 31.8 MB (67.48%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -4,10 +4,10 @@ public:
         if(r<0 || c<0 || r>=n || c>=n || grid[r][c] != val){
             return false;
         }
-        if(val == n*n -1){
+        if(val == n*n - 1){
             return true;
         }
-        //8 possible move
+        // 8 possible move
         bool a1 = isValid(grid,r-2,c+1,n,val+1);
         bool a2 = isValid(grid,r-1,c+2,n,val+1);
         bool a3 = isValid(grid,r+1,c+2,n,val+1);
@@ -19,7 +19,6 @@ public:
         return a1 || a2 || a3 || a4 || a5 || a6 || a7 || a8;
     }
     bool checkValidGrid(vector<vector<int>>& grid) {
-        int n = grid.size();
-        return isValid(grid,0,0,n,0);
+        return isValid(grid,0,0,grid.size(),0);
     }
 };
