48fedf53b67f135c44eeb449efdaa8ff2e0d3622
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-20 20:16:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void PS(vector<int>& nums,vector<vector<int>> &All,vector<int> &ans,int i){
+    void PS(vector<int> &nums,vector<vector<int>> &All,vector<int> &ans,int i){
         if(i == nums.size()){
             All.push_back(ans);
             return;
@@ -8,8 +8,8 @@ public:
         ans.push_back(nums[i]);
         //include
         PS(nums,All,ans,i+1);
-        ans.pop_back();//backtrack
-        //Exclude
+        ans.pop_back();
+        //exlude
         int idx = i+1;
         while(idx < nums.size() && nums[idx] == nums[idx-1]){
             idx++;
@@ -19,7 +19,7 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>> All;
-        vector<int> re;   
+        vector<int> re;
         PS(nums,All,re,0);
         return All;
     }
