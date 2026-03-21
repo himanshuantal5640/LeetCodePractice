d969e337f4be4c9bde18c56dab5cc0df98012d8d
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-21 10:21:43 +0530

    Time: 3 ms (61.46%), Space: 10.3 MB (55.12%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isSafe(vector<string> &chess,int row,int col,int n){
+        //horizontal
+        for(int j=0;j<n;j++){
+            if(chess[row][j] == 'Q'){
+                return false;
+            }
+        }
+        //verticall
+        for(int i=0;i<n;i++){
+            if(chess[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //left diagonal
+        for(int i=row,j=col; i>=0&&j>=0;i--,j--){
+            if(chess[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //right daigonal
+        for(int i=row,j=col;i>=0&&j<n;i--,j++){
+            if(chess[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void NQueens(vector<string> &chess,int row,int n,vector<vector<string>> &ans){
+        if(row == n){
+            ans.push_back(chess);
+            return;
+        }
+        for(int j=0;j<n;j++){
+            if(isSafe(chess,row,j,n)){
+                chess[row][j] = 'Q';
+                NQueens(chess,row+1,n,ans);
+                chess[row][j] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> chess(n,string(n,'.'));
+        vector<vector<string>> ans;
+        NQueens(chess,0,n,ans);
+        return ans;
+    }
+};
