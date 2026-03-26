4e2cb29f926180dd57aba99320658e8857caed44
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-26 20:18:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size();
+        int lm = 0;
+        int rm = 0;
+        int ans = 0;
+        while(l < r){
+            lm = max(lm,height[l]);
+            rm = max(rm,height[r]);
+            if(lm < rm){
+                ans += (lm - height[l]);
+                l++;
+            }
+            else{
+                ans += (rm - height[r]);
+                r--;
+            }
+        }
+        return ans;
+    }
+};
