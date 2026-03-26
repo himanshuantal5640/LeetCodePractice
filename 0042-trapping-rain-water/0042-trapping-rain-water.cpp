077a6a38ad4c0374a060bf34fa469fdee46bebb1
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-03-26 20:18:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int l = 0;
-        int r = height.size();
+        int r = height.size() - 1;
         int lm = 0;
         int rm = 0;
         int ans = 0;
