7e9ceb1f94145cece68edf6d34bb67f2f1925c5a
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-04-29 11:56:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -3,9 +3,9 @@ public:
     string multiply(string num1, string num2) {
         int n = num1.size();
         int m = num2.size();
-        vector<int> res(n+m,0);
-        for(int i = n-1;i>=0;i--){
-            for(int j = m-1;j>=0;j--){
+        vector<int> res(m+n,0);
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
                 int mul = (num1[i] - '0')*(num2[j]-'0');
                 int sum = mul + res[i+j+1];
                 res[i+j+1] = sum%10;
@@ -14,7 +14,7 @@ public:
         }
         string ans = "";
         for(int x:res){
-            if(!(ans.empty() && x == 0)){ //skip leading zeros
+            if(!(ans.empty() && x == 0)){
                 ans += (x + '0');
             }
         }
