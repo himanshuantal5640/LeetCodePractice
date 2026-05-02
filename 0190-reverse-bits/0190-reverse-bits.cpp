1d5a672efbd7db062f5ece5fc0838f89fdbf88c9
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-02 11:28:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        int res = 0;
+        int re = 0;
         for(int i=0;i<32;i++){
-            int bit = n&1; // find last bit
-            res = res << 1; //add one more place to res
-            res = res | bit; // add last bit to res
-            n = n >> 1; // remove last bit from n
+            int bit = n&1;//find last bit
+            re = re<<1;//add more place
+            re = re|bit;//adding last bit
+            n = n>>1;//remove last bit from n
         }
-        return res;
+        return re;
     }
 };
