6282bd9cbfe27366fb7a110bc0f92f7d77fad603
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-05 11:58:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -2,45 +2,29 @@ class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
         int n = queries.size();
-        
-        // Step 1: sort intervals by start
-        sort(intervals.begin(), intervals.end());
-        
-        // Step 2: store queries with index
+        sort(intervals.begin(),intervals.end());
         vector<pair<int,int>> q;
-        for(int i = 0; i < n; i++) {
-            q.push_back({queries[i], i});
+        for(int i=0;i<n;i++){
+            q.push_back({queries[i],i});
         }
-        sort(q.begin(), q.end());
-        
-        // Min heap: {size, right}
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        
-        vector<int> ans(n, -1);
+        sort(q.begin(),q.end());
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> ans(n,-1);
         int i = 0;
-        
-        // Step 3: process queries
-        for(auto &[query, idx] : q) {
-            
-            // Add all intervals with left <= query
-            while(i < intervals.size() && intervals[i][0] <= query) {
+        for(auto &[query,idx]:q){
+            while(i < intervals.size() && intervals[i][0] <= query){
                 int l = intervals[i][0];
                 int r = intervals[i][1];
-                pq.push({r - l + 1, r});
+                pq.push({r-l+1,r});
                 i++;
             }
-            
-            // Remove invalid intervals (right < query)
-            while(!pq.empty() && pq.top().second < query) {
+            while(!pq.empty() && pq.top().second < query){
                 pq.pop();
             }
-            
-            // Top of heap = smallest valid interval
-            if(!pq.empty()) {
+            if(!pq.empty()){
                 ans[idx] = pq.top().first;
             }
         }
-        
         return ans;
     }
 };
