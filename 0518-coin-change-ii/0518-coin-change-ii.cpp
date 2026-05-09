c1b98d163cfcd801045bb93d78e65be80c2fb66d
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-09 14:27:54 +0530

    Time: 51 ms (16.25%), Space: 36.7 MB (38.59%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int i,int tar,vector<int>& in,vector<vector<int>>& dp){
+        if(i >= in.size()){
+            return tar==0?1:0;
+        }
+        if(dp[i][tar] != -1){
+            return dp[i][tar];
+        }
+        int notTake = solve(i+1,tar,in,dp);
+        int take = 0;
+        if(tar >= in[i]){
+            take = solve(i,tar - in[i],in,dp);
+        }
+        return dp[i][tar] = notTake + take;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        return solve(0,amount,coins,dp);
+    }
+};
