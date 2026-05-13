25371e72894492c311205d69ba4807dda27d660a
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-13 11:35:23 +0530

    Time: 0 ms (100%), Space: 9.7 MB (14.56%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool solve(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+        // Pattern exhausted
+        if (j == p.size()) {
+            return i == s.size();
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        bool firstMatch = false;
+        if (i < s.size() && (s[i] == p[j] || p[j] == '.')) {
+
+            firstMatch = true;
+        }
+        bool ans = false;
+        // Check for '*'
+        if (j + 1 < p.size() && p[j + 1] == '*') {
+            // Skip pattern OR use pattern
+            ans = solve(i, j + 2, s, p, dp) ||
+                  (firstMatch && solve(i + 1, j, s, p, dp));
+        }
+        else {
+            ans = firstMatch && solve(i + 1, j + 1, s, p, dp);
+        }
+        return dp[i][j] = ans;
+    }
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(p.size() + 1, -1));
+        return solve(0, 0, s, p, dp);
+    }
+};
