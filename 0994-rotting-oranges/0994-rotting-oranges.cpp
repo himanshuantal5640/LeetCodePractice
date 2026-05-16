89963a8e7877d231349172d20f1bf956175d938d
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-16 11:21:24 +0530

    Time: 3 ms (28.1%), Space: 17.1 MB (39.01%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,45 +3,48 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int,int>,int>> q;//{{row,col},time}
-        int vis[n][m];
+        int ans = 0;
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
                     q.push({{i,j},0});
-                    vis[i][j] = 2;
-                }
-                else{
-                    vis[i][j] = 0;
                 }
             }
         }
-        int time = 0;
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};// for up down right left
+
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            time  = max(time,t);
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            int time = q.front().second;
             q.pop();
-            for(int i = 0;i<4;i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] != 2
-                   && grid[nrow][ncol] == 1){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol] = 2;
-                   }
+            ans = max(ans,time);
+            if(i-1>=0 && !vis[i-1][j] && grid[i-1][j] == 1){
+                q.push({{i-1,j},time+1});
+                vis[i-1][j] = true;
+            }
+            if(i+1<n && !vis[i+1][j] && grid[i+1][j] == 1){
+                q.push({{i+1,j},time+1});
+                vis[i+1][j] = true;
+            }
+            if(j-1>=0 && !vis[i][j-1] && grid[i][j-1] == 1){
+                q.push({{i,j-1},time+1});
+                vis[i][j-1] = true;
+            }
+            if(j+1 < m && !vis[i][j+1] && grid[i][j+1] == 1){
+                q.push({{i,j+1},time+1});
+                vis[i][j+1] = true;
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j] != 2 && grid[i][j] == 1){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     return -1;
                 }
             }
         }
-        return time;
+        return ans;
+
     }
 };
