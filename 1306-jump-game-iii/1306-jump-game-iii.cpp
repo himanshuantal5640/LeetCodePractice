5dd4f6ba10a4e40a94539637cdee4798e8032125
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-17 09:37:20 +0530

    Time: 0 ms (100%), Space: 39.6 MB (31.78%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool DFS(int i,vector<int>& arr,vector<bool>& vis){
+        if(i < 0 ||  i >= arr.size()){
+            return false;
+        }
+        if(vis[i]){
+            return false;
+        }
+        if(arr[i] == 0){
+            return true;
+        }
+        vis[i] = true;
+        return DFS(i + arr[i],arr,vis) || DFS(i - arr[i],arr,vis);
+
+    }
+    bool canReach(vector<int>& arr, int start) {
+       int n = arr.size();
+       vector<bool> vis(n,false);
+       return DFS(start,arr,vis); 
+    }
+};
