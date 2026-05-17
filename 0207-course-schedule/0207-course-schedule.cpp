d9ffb2fa03db7d5957fc6a13e6771b77b410b5f1
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-17 10:13:45 +0530

    Time: 11 ms (21.56%), Space: 20.9 MB (10.6%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    bool dfs(int src,vector<bool> &vis,vector<bool> &re,vector<vector<int>>& adj){
+    bool DFS(int src,vector<bool>& vis,vector<bool>& re,vector<vector<int>>& adj){
         vis[src] = true;
         re[src] = true;
         vector<int> ne = adj[src];
-        for(int v:ne){
+        for(int v: ne){
             if(!vis[v]){
-                if(dfs(v,vis,re,adj)){
+                if(DFS(v,vis,re,adj)){
                     return true;
                 }
             }
-            else if(re[v]){ //Back Edge
+            else if(re[v]){
                 return true;
             }
         }
         re[src] = false;
         return false;
+
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for(auto ne : prerequisites){
-            int u = ne[0];
-            int v = ne[1];
+        for(auto n : prerequisites){
+            int u = n[0];
+            int v = n[1];
             adj[u].push_back(v);
         }
         vector<bool> vis(numCourses,false);
-        vector<bool> re(numCourses,false);
+        vector<bool> re(numCourses,false);//recursion stack for cyc;e detection
         for(int i = 0;i<numCourses;i++){
             if(!vis[i]){
-                if(dfs(i,vis,re,adj)){
+                if(DFS(i,vis,re,adj)){
                     return false;
                 }
             }
