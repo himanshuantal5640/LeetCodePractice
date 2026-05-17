61ffe8a03cb1462b3928deb3b12d24aa479ff80c
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-17 11:17:06 +0530

    Time: 11 ms (21.56%), Space: 20.9 MB (10.6%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -4,7 +4,7 @@ public:
         vis[src] = true;
         re[src] = true;
         vector<int> ne = adj[src];
-        for(int v: ne){
+        for(int v : ne){
             if(!vis[v]){
                 if(DFS(v,vis,re,adj)){
                     return true;
@@ -16,17 +16,16 @@ public:
         }
         re[src] = false;
         return false;
-
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for(auto n : prerequisites){
+        for(auto n:prerequisites){
             int u = n[0];
             int v = n[1];
             adj[u].push_back(v);
         }
         vector<bool> vis(numCourses,false);
-        vector<bool> re(numCourses,false);//recursion stack for cyc;e detection
+        vector<bool> re(numCourses,false);//recursive for cycle detect
         for(int i = 0;i<numCourses;i++){
             if(!vis[i]){
                 if(DFS(i,vis,re,adj)){
