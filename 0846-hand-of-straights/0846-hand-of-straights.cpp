7cfb517b0b1989c63ab5f42bc2926e6d5ef20e09
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-20 15:39:11 +0530

    Time: 33 ms (73.68%), Space: 32.1 MB (73.61%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -4,15 +4,15 @@ public:
         int n = hand.size();
         if(n % groupSize != 0){
             return false;
-        }
+        }   
         map<int,int> mpp;
         for(int i=0;i<n;i++){
             mpp[hand[i]]++;
         }
-        for(auto it : mpp){
+        for(auto it:mpp){
             int st = it.first;
             int freq = it.second;
-            if(freq > 0){
+            if(freq>0){
                 for(int i = 0;i<groupSize;i++){
                     int cur = st + i;
                     if(mpp[cur] < freq){
