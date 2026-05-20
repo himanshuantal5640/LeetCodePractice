9f1febb1adfe1d8934b98e9fffe9296a658c09ae
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-20 11:03:57 +0530

    Time: 53 ms (68.98%), Space: 21.1 MB (58.15%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    //This Brute force using BFS traversal
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+        while(!q.empty()){
+            string str = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            if(str == endWord){
+                return steps;
+            }
+            for(int i=0;i<str.size();i++){
+                char org = str[i];
+                for(char ch = 'a';ch<='z';ch++){
+                    str[i] = ch;
+                    //it is exixt in set
+                    if(st.find(str) != st.end()){
+                        st.erase(str);
+                        q.push({str,steps+1});
+                    }
+                }
+                str[i] = org;
+            }
+        }
+        return 0;
+    }
+};
