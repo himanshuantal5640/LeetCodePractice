eb78bb702eeef871f7c51d90f407e754c88f2fc9
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-20 10:34:58 +0530

    Time: 3 ms (33.16%), Space: 13.4 MB (36.75%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int findParent(int x, vector<int>& parent){
-        if(x == parent[x]){
+    int findParent(int x,vector<int>& parent){
+        if(parent[x] == x){
             return x;
         }
-        return parent[x] = findParent(parent[x],parent);//path Compression
+        return parent[x] = findParent(parent[x],parent);//path compression
     }
-    bool Union(int u,int v, vector<int>& parent){
-        int pu = findParent(u,parent);
+    bool Union(int u,int v,vector<int>& parent){
+        int pu = findParent(u,parent); 
         int pv = findParent(v,parent);
         if(pu == pv){
-            return false;//cycle found
-        }
+            return false;//cycle
+        } 
         parent[pu] = pv;
         return true;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
         vector<int> parent(n+1);
-        for(int i = 0;i<n;i++){
+        for(int i=0;i<=n;i++){
             parent[i] = i;
         }
-        for(auto it : edges){
+        for(auto it:edges){
             int u = it[0];
             int v = it[1];
             if(!Union(u,v,parent)){
