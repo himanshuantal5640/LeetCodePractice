276358e004c5034c4a041b5364346cfade662009
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-23 11:12:23 +0530

    Time: 19 ms (16.47%), Space: 19 MB (17.51%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    set<vector<int>> s;
-    void getAllCombinations(vector<int>& arr,int idx,int tar,vector<int> &combine,vector<vector<int>> &ans){
+    set<vector<int>> st;
+    void solve(int idx,vector<int>& arr,int tar,vector<int>& combine,vector<vector<int>>& ans){
         if(idx == arr.size() || tar < 0){
             return;
         }
         if(tar == 0){
-            if(s.find(combine) == s.end()){
+            if(st.find(combine) == st.end()){
                 ans.push_back(combine);
-                s.insert(combine);
+                st.insert(combine);
             }
         }
         combine.push_back(arr[idx]);
         //single select
-        getAllCombinations(arr,idx+1,tar-arr[idx],combine,ans);
-        //multilple select
-        getAllCombinations(arr,idx,tar-arr[idx],combine,ans);
-        //exclusion
+        solve(idx+1,arr,tar-arr[idx],combine,ans);
+        //multiple select
+        solve(idx,arr,tar-arr[idx],combine,ans);
+        //exclude
         combine.pop_back();
-        getAllCombinations(arr,idx+1,tar,combine,ans);
+        solve(idx+1,arr,tar,combine,ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> combine;
-        getAllCombinations(candidates,0,target,combine,ans);
-        return ans;   
+        solve(0,candidates,target,combine,ans);
+        return ans;
     }
 };
