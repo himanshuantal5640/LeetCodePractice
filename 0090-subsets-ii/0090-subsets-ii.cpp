32dc46ffd3d25da856f49294302ffb154d48e92b
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-24 11:36:14 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.34%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void solve(int idx,vector<int>& nums,vector<int>& curr,vector<vector<int>>& ans){
-        ans.push_back(curr);// store current subset
+        ans.push_back(curr);//store current subset
         for(int i = idx;i < nums.size();i++){
-            if(i > idx && nums[i] == nums[i-1]){
+            if(i>idx && nums[i] == nums[i-1]){
                 continue;
             }
             curr.push_back(nums[i]);//choose
@@ -12,9 +12,9 @@ public:
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         vector<int> curr;
-        sort(nums.begin(),nums.end());
         solve(0,nums,curr,ans);
         return ans;
     }
