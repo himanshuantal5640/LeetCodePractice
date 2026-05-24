41705697779c3cd7704a5e32ebd84a6f620784dd
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-24 12:22:21 +0530

    Time: 822 ms (23.38%), Space: 11.7 MB (29.04%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool DFS(int row,int col,int idx,int n,int m,vector<vector<char>>& board, string word){
+        if(idx == word.size()){
+            return true;
+        }
+        if(row < 0 || row >= n || col < 0 || col >= m || board[row][col] != word[idx]){
+            return false;
+        }
+        char temp = board[row][col];
+        board[row][col] = '#';
+        bool found = (
+            DFS(row+1,col,idx+1,n,m,board,word) || //down
+            DFS(row-1,col,idx+1,n,m,board,word) || //up
+            DFS(row,col+1,idx+1,n,m,board,word) || //right
+            DFS(row,col-1,idx+1,n,m,board,word) // left
+        );
+        board[row][col] = temp; //backtrack
+        return found;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(DFS(i,j,0,n,m,board,word)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
