34930e76264ec88119ddadc97df9722bafae97ad
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-24 12:31:20 +0530

    Time: 818 ms (24.1%), Space: 11.6 MB (29.04%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -7,22 +7,22 @@ public:
         if(row < 0 || row >= n || col < 0 || col >= m || board[row][col] != word[idx]){
             return false;
         }
-        char temp = board[row][col];
+        char ch = board[row][col];
         board[row][col] = '#';
-        bool found = (
-            DFS(row+1,col,idx+1,n,m,board,word) || //down
-            DFS(row-1,col,idx+1,n,m,board,word) || //up
-            DFS(row,col+1,idx+1,n,m,board,word) || //right
-            DFS(row,col-1,idx+1,n,m,board,word) // left
-        );
-        board[row][col] = temp; //backtrack
-        return found;
+        bool found = 
+            DFS(row + 1,col,idx+1,n,m,board,word)||//down
+            DFS(row - 1,col,idx+1,n,m,board,word)|| //up
+            DFS(row,col+1,idx+1,n,m,board,word)|| //right
+            DFS(row,col-1,idx+1,n,m,board,word);//left
+        
+        board[row][col] = ch; //backtrack
+        return found; 
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
                 if(DFS(i,j,0,n,m,board,word)){
                     return true;
                 }
