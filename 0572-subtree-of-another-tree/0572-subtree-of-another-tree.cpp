bbd1f03dac63e5b5db2b4217f722aae6803229c3
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-25 14:11:53 +0530

    Time: 0 ms (100%), Space: 29.1 MB (38.65%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -9,27 +9,21 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    bool isSame(TreeNode* a, TreeNode* b) {
-        if (a == NULL || b == NULL)
+    bool isSame(TreeNode* a,TreeNode* b){
+        if(a == NULL || b == NULL){
             return a == b;
-
-        return (a->val == b->val) &&
-               isSame(a->left, b->left) &&
-               isSame(a->right, b->right);
+        }
+        return (a->val == b->val) && isSame(a->left,b->left) && isSame(a->right,b->right);
     }
-
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (root == NULL)
+        if(root == NULL || subRoot == NULL){
             return false;
-
-        if (isSame(root, subRoot))
+        }
+        if(isSame(root,subRoot)){
             return true;
-
-        return isSubtree(root->left, subRoot) ||
-               isSubtree(root->right, subRoot);
+        }
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
     }
 };
-
