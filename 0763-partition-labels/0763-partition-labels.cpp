7b5fdf0ea6c1e3fd31f7e7dae1141a2fbbc0e201
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-27 11:18:12 +0530

    Time: 0 ms (100%), Space: 8.9 MB (65.89%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> last(26);//store last occurence of all letter
+        vector<int> last(26);//store last occurence of letter
         for(int i=0;i<s.size();i++){
             last[s[i] - 'a'] = i;
         }
         vector<int> ans;
-        int start = 0;
+        int st = 0;
         int end = 0;
         for(int i=0;i<s.size();i++){
-            end = max(end,last[s[i]-'a']);
+            end = max(end,last[s[i] - 'a']);
             if(i == end){
-                ans.push_back(end - start + 1);
-                start = i + 1;
+                ans.push_back(end - st + 1);
+                st = i + 1;
             }
         }
         return ans;
