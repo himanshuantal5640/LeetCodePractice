4a896995a56f116c8c0500839fb7d400f946855c
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-05-28 12:31:33 +0530

    Time: 79 ms (66.81%), Space: 46.2 MB (59.67%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<bool> vis(n,false);
-        vector<int> minD(n,INT_MAX);
-        minD[0] = 0;
-        int re = 0;
-        for(int i=0;i<n;i++){
-            int u = -1;
-            for(int j=0;j<n;j++){
-                if(!vis[j] && (u == -1 || minD[j] < minD[u]) ){
-                    u = j;
-                }
-            }
-            vis[u] = true;
-            re += minD[u];
-            for(int v = 0;v<n;v++){
-                if(!vis[v]){
-                    int d = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
-                    minD[v] = min(minD[v],d);
+        // Min-heap -> {cost, node}
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<bool> vis(n, false);
+        pq.push({0, 0});
+        int cost = 0;
+        int edges = 0;
+        while(edges < n) {
+            auto [wt, node] = pq.top();
+            pq.pop();
+            // Skip if already visited
+            if(vis[node])
+                continue;
+            vis[node] = true;
+            cost += wt;
+            edges++;
+            // Add all neighbors
+            for(int j = 0; j < n; j++) {
+                if(!vis[j]) {
+                    int dist =
+                    abs(points[node][0] - points[j][0]) +
+                    abs(points[node][1] - points[j][1]);
+                    pq.push({dist, j});
                 }
             }
         }
-        return re;
+        return cost;
     }
 };
