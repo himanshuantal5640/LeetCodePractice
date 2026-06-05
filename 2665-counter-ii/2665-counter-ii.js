1256002e6bbe807bf4771dc1b6cf162e272d5c0e
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-06-05 11:06:15 +0530

    Time: 56 ms (22.91%), Space: 55.1 MB (98.93%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -1,8 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
 var createCounter = function(init) {
-    let presentCount = init
+    let pre = init;
     return {
-        increment:()=> ++presentCount,
-        decrement:()=> --presentCount,
-        reset:()=> presentCount = init,
+        increment:  ()=> ++pre,
+        decrement: ()=> --pre,
+        reset:() => pre = init
     }
 };
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
