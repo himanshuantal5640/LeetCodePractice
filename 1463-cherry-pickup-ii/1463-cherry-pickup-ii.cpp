705d053f045988bf74be66355931884961323dcb
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-06-19 12:02:19 +0530

    Time: 46 ms (65.35%), Space: 20.9 MB (53.35%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int solve(int i,int j1,int j2,int n,int m,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp){
-        //out of bound basecase
+        //out of bound base case
         if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m){
             return -1e8;
         }
-        //destination base case
+        //destination case
         if(i == n-1){
             if(j1 == j2){
                 return grid[i][j1];
             }
             else{
-                return grid[i][j1] + grid[i][j2];
+                return grid[i][j1] +grid[i][j2];
             }
         }
         if(dp[i][j1][j2] != -1){
             return dp[i][j1][j2];
         }
-        //explore all 9 path
-        int maxi = 0;
-        for(int dj1 = -1;dj1 <= 1;dj1++){
-            for(int dj2 = -1;dj2 <= 1;dj2++){
+        //explore all path
+        int maxi = -1e8;
+        for(int dj1 = -1;dj1<=1;dj1++){
+            for(int dj2 = -1;dj2<=1;dj2++){
                 int val = 0;
                 if(j1 == j2){
                     val = grid[i][j1];
@@ -28,12 +28,11 @@ public:
                 else{
                     val = grid[i][j1] + grid[i][j2];
                 }
-                val += solve(i+1,j1+dj1,j2+dj2,n,m,grid,dp);
-                maxi = max(maxi,val);
+                val += solve(i+1,dj1+j1,dj2+j2,n,m,grid,dp);
+                maxi = max(val,maxi);
             }
         }
         return dp[i][j1][j2] = maxi;
-
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
