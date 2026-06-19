780c61cc0ad5b02e5593d8789ba8401727dc2a8d
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-06-19 08:51:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(int n,vector<int>& dp){
-        if(n==0 || n == 1){
+        if(n == 0 || n == 1){
             return 1;
         }
         if(dp[n] != -1){
