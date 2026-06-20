ab6204da7e0b7986a6e0b40174565d4e29bed85b
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-06-20 10:54:27 +0530

    Time: 163 ms (26.57%), Space: 111.4 MB (13.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool solve(int i,int tar,vector<int>& nums,vector<vector<int>>& dp){
-        if(i >= nums.size()){
-            return false;
-        }
-        if(tar == 0){
+    bool solve(int i,int s,int n,vector<int>& nums,vector<vector<int>>& dp){
+        if(s == 0){
             return true;
         }
-        if(dp[i][tar] != -1){
-            return dp[i][tar];
+        if(i >= n){
+            return false;
         }
-        bool notTake = solve(i+1,tar,nums,dp);
+        if(dp[i][s] != -1){
+            return dp[i][s];
+        }
+        bool notTake = solve(i+1,s,n,nums,dp);
         bool take = false;
-        if(tar >= nums[i]){
-            take = solve(i+1,tar-nums[i],nums,dp);
+        if(s >= nums[i]){
+            take = solve(i+1,s - nums[i],n,nums,dp);
         }
-        return dp[i][tar] = take || notTake;
+        return dp[i][s] = notTake || take;
     }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
@@ -23,11 +23,11 @@ public:
         for(int x:nums){
             sum += x;
         }
-        if(sum%2 != 0){
+        if(sum % 2 != 0){
             return false;
         }
-        int s = sum/2;
-        vector<vector<int>> dp(n,vector<int>(s+1,-1));
-        return solve(0,s,nums,dp);
+        int s =sum/2;
+        vector<vector<int>> dp(n+1,vector<int>(s+1,-1));
+        return solve(0,s,n,nums,dp);
     }
 };
