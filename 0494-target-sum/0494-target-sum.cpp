64e889dd241849f61ccdbe060263b63f181c5495
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-03 12:17:47 +0530

    Time: 10 ms (58.21%), Space: 15.6 MB (43.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int solve(int i,int tar,vector<int>& nums,vector<vector<int>>& dp){
-        if(i >= nums.size()){
-            return tar==0?1:0;
+    int solve(int i,int tar,vector<int>& arr,vector<vector<int>>& dp){
+        if(i >= arr.size()){
+            return tar == 0 ? 1:0;
         }
         if(dp[i][tar] != -1){
             return dp[i][tar];
         }
-        int notTake = solve(i+1,tar,nums,dp);
-        int take = 0;
-        if(tar >= nums[i]){
-            take = solve(i+1,tar-nums[i],nums,dp);
+        int notPick = solve(i+1,tar,arr,dp);
+        int pick = 0;
+        if(tar >= arr[i]){
+            pick = solve(i+1,tar - arr[i],arr,dp);
         }
-        return dp[i][tar] = take + notTake;
+        return dp[i][tar] = pick + notPick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int sum = 0;
-        for(int x:nums){
-            sum += x;
+        for(auto it:nums){
+            sum += it;
         }
-        if(abs(target) > sum || (target + sum)%2 != 0){
+        if(abs(target) > sum || (sum + target)%2 != 0){
             return 0;
         }
-        int newTar = (sum+target)/2;
-        vector<vector<int>> dp(n,vector<int>(newTar+1,-1));
+        int newTar = (sum + target)/2;
+        vector<vector<int>> dp(n+1,vector<int>(newTar+1,-1));
         return solve(0,newTar,nums,dp);
     }
 };
