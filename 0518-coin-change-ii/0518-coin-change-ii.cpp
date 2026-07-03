97b76dc44498a2d9ebcd0326f7300a2df261b55e
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-03 12:32:56 +0530

    Time: 49 ms (17.98%), Space: 36.9 MB (28.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solve(int i,int tar,vector<int>& in,vector<vector<int>>& dp){
-        if(i >= in.size()){
-            return tar == 0? 1:0;
+    int solve(int i,int tar,vector<int>& arr,vector<vector<int>>& dp){
+        if(i >= arr.size()){
+            return tar == 0 ? 1:0;
         }
         if(dp[i][tar] != -1){
             return dp[i][tar];
         }
-        int notTake = solve(i+1,tar,in,dp);
-        int take = 0;
-        if(tar >= in[i]){
-            take = solve(i,tar - in[i],in,dp);
+        int notPick = solve(i+1,tar,arr,dp);
+        int pick = 0;
+        if(tar >= arr[i]){
+            pick = solve(i,tar-arr[i],arr,dp);
         }
-        return dp[i][tar] = notTake + take;
+        return dp[i][tar] = notPick+pick;
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
         return solve(0,amount,coins,dp);
     }
 };
