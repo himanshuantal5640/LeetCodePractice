043669bfaa738568d31536acb37e5c6768b8db69
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-08 12:21:00 +0530

    Time: 15 ms (31.6%), Space: 16.6 MB (74.95%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int i,int j,string& s1,string& s2,vector<vector<int>>& dp){
+        if(i >= s1.size() || j >= s2.size()){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s1[i] == s2[j]){
+            return dp[i][j] = 1 + solve(i+1,j+1,s1,s2,dp);
+        }
+        return dp[i][j] = max(solve(i+1,j,s1,s2,dp),solve(i,j+1,s1,s2,dp));
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        int total = n + m;
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        int lcs = solve(0,0,word1,word2,dp);
+        return total - 2*lcs;
+    }
+};
