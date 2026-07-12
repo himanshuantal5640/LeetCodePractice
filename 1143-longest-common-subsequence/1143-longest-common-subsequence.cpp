e93eab5bdf68f79748283fc204d39d55d3c0b295
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-12 11:10:58 +0530

    Time: 43 ms (25.02%), Space: 27.5 MB (30.92%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(int i,int j,string& s1,string& s2,vector<vector<int>>& dp){
-        if(i == s1.size() || j == s2.size()){
+        if(i >= s1.size() || j >= s2.size()){
             return 0;
         }
         if(dp[i][j] != -1){
