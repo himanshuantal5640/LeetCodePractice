b461856c8ff38b35d396e22473e910b3653cf259
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-13 20:22:49 +0530

    Time: 0 ms (100%), Space: 16.4 MB (13.36%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int solve(int i,int buy,vector<int>& in, vector<vector<int>>& dp){
-        if(i >= in.size()){
+    int solve(int idx,int buy,vector<int>& arr,vector<vector<int>>& dp){
+        if(idx >= arr.size()){
             return 0;
         }
-        if(dp[i][buy] != -1){
-            return dp[i][buy];
+        if(dp[idx][buy] != -1){
+            return dp[idx][buy];
         }
         int profit = 0;
         if(buy){
-            profit = max(-in[i]+solve(i+1,0,in,dp),solve(i+1,1,in,dp));
+            profit = max(-arr[idx] + solve(idx+1,0,arr,dp),solve(idx+1,1,arr,dp));
         }
         else{
-            profit = max(in[i]+solve(i+2,1,in,dp),solve(i+1,0,in,dp));
+            //sel
+            profit = max(arr[idx] + solve(idx+2,1,arr,dp),solve(idx+1,0,arr,dp));
         }
-        return dp[i][buy] = profit;
+        return dp[idx][buy] = profit;
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
         return solve(0,1,prices,dp);
     }
 };
