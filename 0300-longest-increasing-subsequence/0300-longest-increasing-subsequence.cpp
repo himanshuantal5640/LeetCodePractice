d8d9d78b84c637522649ca1598b9e899e1a712a2
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-18 10:35:08 +0530

    Time: 573 ms (12.53%), Space: 296.6 MB (13.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -7,11 +7,12 @@ public:
         if(dp[i][prev+1] != -1){
             return dp[i][prev+1];
         }
-        int len = solve(i+1,prev,arr,dp);//not take
+        int notTake = solve(i+1,prev,arr,dp);
+        int take = 0;
         if(prev == -1 || arr[i] > arr[prev]){
-            len = max(len,1+solve(i+1,i,arr,dp));
+            take = 1 + solve(i+1,i,arr,dp);
         }
-        return dp[i][prev+1] = len;
+        return dp[i][prev+1] = max(take,notTake);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
