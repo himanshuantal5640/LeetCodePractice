888e289a4a0dcf0e49cf959448232493c34d72ae
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-18 11:17:32 +0530

    Time: 167 ms (30.99%), Space: 106.1 MB (26.63%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -6,7 +6,7 @@ public:
         }
         int i = 0;
         int j = 0;
-        while(i < a.size() && j < b.size()){
+        while(i<a.size() && j<b.size()){
             if(a[i] == b[j]){
                 i++;
                 j++;
@@ -14,31 +14,28 @@ public:
             else{
                 j++;
             }
-            
         }
         return i == a.size();
     }
-    int solve(int i,int prev,vector<string>& s,vector<vector<int>>& dp){
-        if(i >= s.size()){
+    int solve(int i,int prev,vector<string>& arr,vector<vector<int>>& dp){
+        if(i >= arr.size()){
             return 0;
         }
         if(dp[i][prev+1] != -1){
             return dp[i][prev+1];
         }
-        int notTake = solve(i+1,prev,s,dp);
+        int notTake = solve(i+1,prev,arr,dp);
         int take = 0;
-        if(prev == -1 || isPreced(s[prev],s[i])){
-            take = 1 + solve(i+1,i,s,dp);
+        if(prev == -1 || isPreced(arr[prev],arr[i])){
+            take = 1 + solve(i+1,i,arr,dp);
         }
         return dp[i][prev+1] = max(take,notTake);
     }
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        //sort by length
         sort(words.begin(),words.end(),[](string& a,string& b){
             return a.size() < b.size();
         });
-
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         return solve(0,-1,words,dp);
     }
