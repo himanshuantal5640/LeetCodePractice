e9b5622067ae4031d176cb7d2051acfebbb9cda6
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-21 12:13:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.74%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool checkValidString(string s) { 
+    bool checkValidString(string s) {
         int low = 0;
-        int high = 0;    
-        for(char ch : s) {        
-            if(ch == '(') {
+        int high = 0;
+        for(char ch:s){
+            if(ch == '('){
                 low++;
                 high++;
-            }          
-            else if(ch == ')') {
+            }
+            else if(ch == ')'){
                 low--;
                 high--;
             }
-            else { // '*'
+            else{//*
                 low--;
                 high++;
             }
-            low = max(low, 0);
-            if(high < 0)
+            low = max(low,0);
+            if(high < 0){
                 return false;
+            }
         }
-        
         return low == 0;
     }
 };
