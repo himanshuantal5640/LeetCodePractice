b2878e28caba4f72a5996ae5643159446d1b3e02
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-30 15:18:21 +0530

    Time: 3 ms (40.84%), Space: 9.2 MB (82.83%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char,char> mp1; // s -> t
+        unordered_map<char,char> mp2; // t -> s
+        for(int i=0;i<s.size();i++){
+            char c1 = s[i];
+            char c2 = t[i];
+            if(mp1.find(c1) != mp1.end() && mp1[c1] != c2){
+                return false;
+            }
+            if(mp2.find(c2) != mp2.end() && mp2[c2] != c1){
+                return false;
+            }
+            mp1[c1] = c2;
+            mp2[c2] = c1;
+        }
+        return true;
+    }
+};
