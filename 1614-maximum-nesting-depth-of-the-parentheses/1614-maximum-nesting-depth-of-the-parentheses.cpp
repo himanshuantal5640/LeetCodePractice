035deffb857a6b117b98f44617c6eb182a26f7aa
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-07-31 11:59:35 +0530

    Time: 0 ms (100%), Space: 8.3 MB (86.52%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0;
+        int maxi = 0;
+        for(char ch : s){
+            if(ch == '('){
+                depth++;
+                maxi = max(depth,maxi);
+            }
+            else if(ch == ')'){
+                depth--;
+            }
+        }
+        return maxi;
+    }
+};
