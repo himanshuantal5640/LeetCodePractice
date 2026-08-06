4e2b97c044482a97002d3ec85b79c4786dbd558a
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-06 18:26:28 +0530

    Time: 6 ms (88.25%), Space: 9.4 MB (77.61%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -5,19 +5,19 @@ public:
             l--;
             r++;
         }
-        return r - l -1;
+        return r - l - 1;
     }
     string longestPalindrome(string s) {
         int n = s.size();
         int st = 0;
-        int maxi = 1;
+        int maxi = 0;
         for(int i=0;i<n;i++){
             int l1 = expand(s,i,i);//odd
             int l2 = expand(s,i,i+1);//even
             int len = max(l1,l2);
             if(len > maxi){
                 maxi = len;
-                st = i -(len-1)/2;
+                st = i - (len - 1)/2;
             }
         }
         return s.substr(st,maxi);
