09fabf8088121da9bae77981dbfe641ac8be82a9
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-10 11:14:54 +0530

    Time: 0 ms (100%), Space: 21 MB (34.49%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,7 +5,7 @@ public:
         int m = matrix[0].size();
         vector<int> row(n,0);
         vector<int> col(m,0);
-        for(int i=0;i<n;i++){
+        for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(matrix[i][j] == 0){
                     row[i] = 1;
@@ -13,7 +13,7 @@ public:
                 }
             }
         }
-        for(int i = 0;i<n;i++){
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(row[i] == 1 || col[j] == 1){
                     matrix[i][j] = 0;
