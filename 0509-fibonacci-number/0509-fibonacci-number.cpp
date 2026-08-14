5dfb467b4978902ba79bfaeeeebc269902850ede
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-14 15:40:57 +0530

    Time: 15 ms (22.69%), Space: 7.9 MB (38.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-    int fibo(int n){
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1){
-            return 1;
-        }
-        int sum = fibo(n-1) + fibo(n-2);
-        return sum;
-    }
     int fib(int n) {
-        return fibo(n);
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+
     }
 };
