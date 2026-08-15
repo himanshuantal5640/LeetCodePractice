3bca19891c3b1c73365b50effff628e88570c1d9
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-15 11:46:00 +0530

    Time: 0 ms (100%), Space: 9 MB (98.5%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:
+    int solve(int i,string& s,long long num,int sign){
+        //base
+        if(i >= s.size() || !isdigit(s[i])){
+            long long ans = num * sign;
+            if(ans > INT_MAX){
+                return INT_MAX;
+            }
+            else if(ans < INT_MIN){
+                return INT_MIN;
+            }
+            return (int)ans;
+        }
+        int digit = s[i] - '0';
+        //overflow
+        if(num > (INT_MAX - digit)/10){
+            if(sign == 1){
+                return INT_MAX;
+            }
+            else{
+                return INT_MIN;
+            }
+        }
+        num = num*10 + digit;
+        return solve(i+1,s,num,sign);
+    }
     int myAtoi(string s) {
-        int i = 0;
         int n = s.size();
+        int i = 0;
         //skip space
-        while(i<n && s[i] == ' '){
+        while(i < n && s[i] == ' '){
             i++;
-        }   
+        }
+        if(i == n){
+            return 0;
+        }
         int sign = 1;
-        if(i < n && (s[i] == '-' || s[i] == '+')){
-            if(s[i] == '-'){
-                sign = -1;
-            }
+        if(s[i] == '-'){
+            sign = -1;
             i++;
         }
-        long long ans = 0;
-        while(i < n && isdigit(s[i])){
-            int d = s[i] - '0';
-            if(ans > INT_MAX/10 || (ans == INT_MAX/10 && d > 7)){
-                if(sign == 1){
-                    return INT_MAX;
-                }
-                else{
-                    return INT_MIN;
-                }
-            }
-            ans = ans * 10 + d;
+        else if(s[i] == '+'){
             i++;
         }
-        return ans*sign;
+
+        if(i == n || !isdigit(s[i])){
+            return 0;
+        }
+        return solve(i,s,0,sign);
     }
 };
