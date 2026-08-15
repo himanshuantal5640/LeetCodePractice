503c8c8bb128d3eadf4a9130f0d4b9b96d73c185
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-15 12:13:09 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+    double pow(double x,long long n){
+        if(n == 0){
+            return 1;
+        }
+        //recursive call
+        double half = pow(x,n/2);
+        //even power
+        if(n % 2 == 0){
+            return half*half;
+        }
+        else{
+            return x * half * half;
+        }
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long N = n;
+        if(N > 0){
+            return pow(x,N);
+        }
+        //negative power
+        return 1.0/pow(x,-N);
     }
 };
