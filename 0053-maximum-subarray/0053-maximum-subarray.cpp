3076a5e37e2f3ae024c0efbc9e97d737bf364a15
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-15 19:33:52 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.84%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,12 +5,10 @@ public:
         int maxi = INT_MIN;
         int sum = 0;
         for(int i=0;i<n;i++){
-            sum += nums[i];
+            sum = max(nums[i],sum+nums[i]);
             maxi = max(maxi,sum);
-            if(sum < 0){
-                sum = 0;
-            }
         }
         return maxi;
+        
     }
 };
