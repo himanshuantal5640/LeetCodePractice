63418a5e51a1df4e3b490e690bec923661c6c264
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-15 12:44:50 +0530

    Time: 0 ms (100%), Space: 7.8 MB (71.82%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     long long mod = 1e9 + 7;
-    long long solve(long long i,long long n){
-        //base case
-        if(n == 0){
+    long long solve(long long x,long long n){
+        if(n==0){
             return 1;
         }
-        long long half = solve(i,n/2);
+        long long half = solve(x,n/2);
         if(n%2 == 0){
             return (half*half)%mod;
         }
-        return (i*half*half)%mod;
+        return (x*half*half)%mod;
     }
     int countGoodNumbers(long long n) {
         long long even = (n+1)/2;
         long long odd = n/2;
-        long long ans = solve(5,even);//even index 5 option
-        ans = (ans* solve(4,odd))%mod;//odd index 4 option
+        long long ans = solve(5,even);//even index
+        ans = (ans * solve(4,odd))%mod; //odd index
         return ans;
     }
 };
