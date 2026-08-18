2d137b7148112a74b60913fb847ab399fb24f571
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-18 11:54:56 +0530

    Time: 10 ms (20.17%), Space: 19.3 MB (17.32%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     set<vector<int>> st;
-    void solve(int idx,vector<int>& arr,int tar,vector<int>& combine,vector<vector<int>>& ans){
-        if(idx == arr.size() || tar < 0){
+    void solve(int i,int tar,vector<int>& arr,vector<int>& combine,vector<vector<int>>& ans){
+        if(i >= arr.size() || tar < 0){
             return;
         }
         if(tar == 0){
@@ -11,19 +11,19 @@ public:
                 st.insert(combine);
             }
         }
-        combine.push_back(arr[idx]);
-        //single
-        solve(idx+1,arr,tar-arr[idx],combine,ans);
-        //multiple
-        solve(idx,arr,tar-arr[idx],combine,ans);
-        //exclude
+        combine.push_back(arr[i]);
+        //include next
+        solve(i+1,tar - arr[i],arr,combine,ans);
+        //include same
+        solve(i,tar - arr[i],arr,combine,ans);
+        //exlcude
         combine.pop_back();
-        solve(idx+1,arr,tar,combine,ans);
+        solve(i+1,tar,arr,combine,ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> combine;
-        solve(0,candidates,target,combine,ans);
+        solve(0,target,candidates,combine,ans);
         return ans;
     }
 };
