7d86692102909afb336d02e4f2c3457cb603d11b
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-08-20 10:03:23 +0530

    Time: 804 ms (25.14%), Space: 11.5 MB (43.06%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,25 +4,25 @@ public:
         if(idx == word.size()){
             return true;
         }
-        if(row < 0 || row >= n || col < 0 || col >= m || board[row][col] != word[idx]){
+        if(row < 0 || col < 0 || row >= n || col >= m || board[row][col] != word[idx]){
             return false;
         }
         char ch = board[row][col];
         board[row][col] = '#';
-        bool found = 
-            DFS(row + 1,col,idx+1,n,m,board,word)||//down
-            DFS(row - 1,col,idx+1,n,m,board,word)|| //up
-            DFS(row,col+1,idx+1,n,m,board,word)|| //right
-            DFS(row,col-1,idx+1,n,m,board,word);//left
-        
-        board[row][col] = ch; //backtrack
-        return found; 
+        bool found = (
+            DFS(row+1,col,idx+1,n,m,board,word) ||
+            DFS(row-1,col,idx+1,n,m,board,word) ||
+            DFS(row,col+1,idx+1,n,m,board,word) ||
+            DFS(row,col-1,idx+1,n,m,board,word) 
+        );
+        board[row][col] = ch;//backtrack
+        return found;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(DFS(i,j,0,n,m,board,word)){
                     return true;
                 }
