d53faa9acb00c72843aa082a693c00bb52869e58
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-09-22 13:11:56 +0530

    Time: 7 ms (99.69%), Space: 139.2 MB (49.59%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,17 +4,18 @@ public:
         deque<int> dq;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            //remove element outside window
-            while(!dq.empty() && dq.front() <= i - k){
+            //remove outside window
+            while(!dq.empty() && dq.front() <= i-k){
                 dq.pop_front();
             }
-            //remove smaller element
+            //remove smaller elemment
             while(!dq.empty() && nums[dq.back()] <= nums[i]){
                 dq.pop_back();
             }
             //push current index
             dq.push_back(i);
-            if(i >= k - 1){
+
+            if(i >= k-1){
                 ans.push_back(nums[dq.front()]);
             }
         }
