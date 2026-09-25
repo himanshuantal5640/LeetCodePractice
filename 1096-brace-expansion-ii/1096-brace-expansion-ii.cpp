9aa16a9208f0f656fd3bc2c4bb89906277e26e62
himanshu-antal <149318002+himanshuantal5640@users.noreply.github.com>
2026-09-25 09:15:31 +0530

    Time: 15 ms (26.34%), Space: 15.9 MB (45.09%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.cpp
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.cpp
@@ -1,71 +1,38 @@
 class Solution {
 public:
-    vector<char> op;
-    vector<vector<string>> st;
-
-    void perf() {
-        int x=st.size()-2, y = st.size()-1;
-        if (op.back()=='+'){
-            // Union
-            st[x].reserve(st[x].size()+st[y].size());
-            st[x].insert(st[x].end(), st[y].begin(), st[y].end());
-        }
-        else { // op.back()=='*'
-            // Concatenation
-            vector<string> nxt;
-            nxt.reserve(st[x].size()*st[y].size());
-            for (const auto& l : st[x]) {
-                for (const auto& r : st[y]) 
-                    nxt.push_back(l+r);
-            }
-            st[x]=move(nxt);
-        }
-        op.pop_back();
-        st.pop_back();
+    vector<string> braceExpansionII(string expression) {
+        int i = 0;
+        set<string> res = parse(expression, i);
+        return vector<string>(res.begin(), res.end());
     }
-    vector<string> braceExpansionII(string& expr) {
-        const int n=expr.size();
-        op.reserve(n);
-
-        char prv='@', cur;
-
-        for (int i=0; i<n; i++, prv=cur) {
-            cur = expr[i];
-            switch (cur) {
-            case ',': 
-                while (!op.empty() && op.back() != '{') perf();
-                op.push_back('+');
-                break;
-            case '{':
-                if (prv=='}' || isalpha(prv)) op.push_back('*');
-                op.push_back('{');
-                break;
-            case '}':
-                while (!op.empty() && op.back()!='{') perf();
-                op.pop_back(); // Remove matching '{'
-                break;
-            default:
-                if (prv=='}') op.push_back('*');
-                string s;
-                for (; i<n && isalpha(expr[i]); i++) s+=expr[i];
-                st.push_back({s});
-                i--;
-                cur=expr[i];
+private:
+    set<string> parse(const string& expr, int& i) {
+        set<string> res;
+        set<string> cur{""};
+        while (i < expr.size() && expr[i] != '}') {
+            if (expr[i] == '{') {
+                ++i;
+                set<string> next = parse(expr, i);
+                ++i;
+                cur = product(cur, next);
+            } else if (expr[i] == ',') {
+                res.insert(cur.begin(), cur.end());
+                cur = {""};
+                ++i;
+            } else {
+                set<string> next{string(1, expr[i])};
+                ++i;
+                cur = product(cur, next);
             }
         }
-
-        while (!op.empty()) perf();
-
-        auto ans=st.front();
-        sort(ans.begin(), ans.end());
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
-        return ans;
+        res.insert(cur.begin(), cur.end());
+        return res;
+    }
+    set<string> product(const set<string>& a, const set<string>& b) {
+        set<string> res;
+        for (const string& x : a)
+            for (const string& y : b)
+                res.insert(x + y);
+        return res;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
